678e20b20724c872dbd2e4f65f6a4afa1fc5eb09
BayaYi <75089683+BayaYi@users.noreply.github.com>
2024-02-29 19:53:59 +0300

    s

---
+++ bendersArenaFinal/Assets/Scripts/TourMechanism.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourMechanism : MonoBehaviour
+{
+    public int totalPlayerNumber;
+    public int totalNonPlayerNumber;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
