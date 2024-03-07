2a8518fc48028233bca5376e53c2dd776629f939
BayaYi <75089683+BayaYi@users.noreply.github.com>
2024-03-07 17:39:26 +0300

    kod

---
+++ bendersArenaFinal/Assets/Scripts/BaseStats.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStats : MonoBehaviour
+{
+    
+        
+}
+public struct BaseStat 
+    {
+        //Temel Statlar
+        public int strength;
+        public int dexterity;
+        public int constitution;
+        public int intelligence;
+        public int physicalPower;
+        public int chiPower;
+        public int charisma;
+        //Türetilmiþ Statlar
+        public int health;
+        public int physicalEnergy;
+        public int chiEnergy;
+        public int speed;
+        public int physicalDefence;
+        public int bendingDefence;
+        public int initiative;
+    }
