7d36d29d3595913ee3d13d84301dd4f036e88832
BayaYi <75089683+BayaYi@users.noreply.github.com>
2024-03-10 14:44:50 +0300

    get set

--- bendersArenaFinal/Assets/Scripts/BaseStats.cs
+++ bendersArenaFinal/Assets/Scripts/BaseStats.cs
@@ -10,27 +10,27 @@ public class BaseStats : MonoBehaviour
 public struct BaseStat 
 {
     //Temel Statlar
-    public int strength;
-    public int dexterity;
-    public int constitution;
-    public int intelligence;
-    public int physicalPower;
-    public int chiPower;
-    public int charisma;
+    public int strength { get; set; }
+    public int dexterity { get; set; }
+    public int constitution { get; set; }
+    public int intelligence { get; set; }
+    public int physicalPower { get; set; }
+    public int chiPower { get; set; }
+    public int charisma { get; set; }
     //Türetilmiþ Statlar
-    public int totalHealth;
-    public int totalPhysicalEnergy;
-    public int TotalChiEnergy;
-    public int totalSpeed;
-    public int totalPhysicalDefence;
-    public int totalBendingDefence;
-    public int totalInitiative;
+    public int totalHealth { get; set; }
+    public int totalPhysicalEnergy { get; set; }
+    public int TotalChiEnergy { get; set; }
+    public int totalSpeed { get; set; }
+    public int totalPhysicalDefence { get; set; }
+    public int totalBendingDefence { get; set; }
+    public int totalInitiative { get; set; }
     //Güncel Durumlar
-    public int currentHealth;
-    public int currentPhysicalEnergy;
-    public int currentChiEnergy;
-    public int currentSpeed;
-    public int currentPhysicalDefence;
-    public int currentBendingDefence;
-    public int currentInitiative;
+    public int currentHealth { get; set; }
+    public int currentPhysicalEnergy { get; set; }
+    public int currentChiEnergy { get; set; }
+    public int currentSpeed { get; set; }
+    public int currentPhysicalDefence { get; set; }
+    public int currentBendingDefence { get; set; }
+    public int currentInitiative { get; set; }
 }
