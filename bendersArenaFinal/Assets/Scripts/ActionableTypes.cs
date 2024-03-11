fdb04814b4d39ab372a2893e70f5c06c7d0c4f91
Umutdmrl0 <umutd.work@gmail.com>
2024-03-11 14:29:19 +0300

    Squashed commit of the following:
    commit b4ceb1afe3a3da380cc05857fe5efc57c2430849
    Merge: 85f25be d7f5074
    Author: BayaYi <75089683+BayaYi@users.noreply.github.com>
    Date:   Sun Mar 10 14:47:04 2024 +0300
    
        Merge pull request #28 from BayaYi/EyyupChan
    
        terrain added
    
    commit d7f507408770aa367f8c74b6c2499f78563e6567
    Author: BayaYi <75089683+BayaYi@users.noreply.github.com>
    Date:   Sun Mar 10 14:46:47 2024 +0300
    
        terrain added
    
    commit 85f25be040ecaafe536c1504410983c1aace893c
    Merge: 2e52de1 7d36d29
    Author: BayaYi <75089683+BayaYi@users.noreply.github.com>
    Date:   Sun Mar 10 14:45:05 2024 +0300
    
        Merge pull request #27 from BayaYi/EyyupChan
    
        get set
    
    commit 7d36d29d3595913ee3d13d84301dd4f036e88832
    Author: BayaYi <75089683+BayaYi@users.noreply.github.com>
    Date:   Sun Mar 10 14:44:50 2024 +0300
    
        get set

--- bendersArenaFinal/Assets/Scripts/ActionableTypes.cs
+++ bendersArenaFinal/Assets/Scripts/ActionableTypes.cs
@@ -4,29 +4,56 @@ using UnityEngine;
 
 public class ActionableTypes : MonoBehaviour
 {
-    
-}
-public struct Bender 
+    public struct Bender 
 {
     public BaseStat stats;
-    public string name;
-    public string bending;
-    public bool isServerFounder;
-    public int playerNumber;
+    public string name { get; set; }
+    public string bending { get; set; }
+    public bool isServerFounder { get; set; }
+    public int playerNumber { get; set; }
 }
-public struct Monster
+    public struct Monster
 {
     public BaseStat stats;
-    public string name;
-    public bool isMagical;
-    public string armourType;
+    public string name { get; set; }
+    public bool isMagical { get; set; }
+    public string armourType { get; set; }
 }
-public struct Weapon
-{
-    public bool isMelee;
-    public string damageType;
-    public int damageDice;
-    public int diceNumber;
+    public struct Weapon
+    {
+        public bool isMelee { get; set; }
+        public string damageType { get; set; }
+        public int damageDice { get; set; }
+        public int diceNumber { get; set; }
+    }
+    void shortBow(Weapon weapon)
+    {
+        weapon.isMelee = false;
+        weapon.damageType = "pierce";
+        weapon.damageDice = 6;
+        weapon.diceNumber = 2;
+    }
+    void longBow(Weapon weapon)
+    {
+        weapon.isMelee=false;
+        weapon.damageType = "pierce";
+        weapon.damageDice = 4;
+        weapon.diceNumber = 2;
+    }
+    void shortSword(Weapon weapon)
+    {
+        weapon.isMelee = true;
+        weapon.damageType = "slashing";
+        weapon.damageDice = 8;
+        weapon.diceNumber = 2;
+    }
+    void longSword(Weapon weapon)
+    {
+        weapon.isMelee = false;
+        weapon.damageType = "slashing";
+        weapon.damageDice = 10;
+        weapon.diceNumber = 2;
+    }
 }
 public class ShortBow
 {
