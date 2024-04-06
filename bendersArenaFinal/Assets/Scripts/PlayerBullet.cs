9c433ef7198247d675c9880feffd65ced0909bf3
BayaYi <75089683+BayaYi@users.noreply.github.com>
2024-04-06 17:35:45 +0300

    Mermi Hedef yönünde yaratma V.1

---
+++ bendersArenaFinal/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public Vector3 bulletLocation = Vector3.zero;
+
+    public void Konum()
+    {
+        //Belirtilen etikete sahip nesneyi bulan ve yeni bir obje olarak atandý
+        GameObject playerNesnesi = GameObject.FindGameObjectWithTag("Player");
+        GameObject enemyNesnesi = GameObject.FindGameObjectWithTag("Enemy");
+
+        //Objelerin Konumlarý bulundu.
+        Vector3 playerKonumu = playerNesnesi.transform.position;
+        Vector3 enemyKonumu = enemyNesnesi.transform.position;
+
+        //Konum farklarý mutlak deðer cinsinden belirlendi ve en yüksek farka göre merminin konumlanacaðý yer hesaplandý.
+        float xfark = Mathf.Abs(playerKonumu.x - enemyKonumu.x);
+        float yfark = Mathf.Abs(playerKonumu.y - enemyKonumu.y);
+        float zfark = Mathf.Abs(playerKonumu.z - enemyKonumu.z);
+
+        if (xfark > yfark && xfark > zfark)
+        {
+            if (playerKonumu.x > enemyKonumu.x)
+            {
+                bulletLocation = Vector3.left + playerKonumu;
+            }
+            else
+            {
+                bulletLocation = Vector3.right + playerKonumu;
+            }
+        }
+        else if (yfark > xfark && yfark > zfark)
+        {
+            if (playerKonumu.y > enemyKonumu.y)
+            {
+                bulletLocation = Vector3.down + playerKonumu;
+            }
+            else
+            {
+                bulletLocation = Vector3.up + playerKonumu;
+            }
+        }
+        else
+        {
+            if (playerKonumu.z > enemyKonumu.z)
+            {
+                bulletLocation = Vector3.back + playerKonumu;
+            }
+            else
+            {
+                bulletLocation = Vector3.forward + playerKonumu;
+            }
+        }
+        //Mermi oluþturuldu.
+        Instantiate(bullet, bulletLocation, Quaternion.identity);
+
+    }
+}
