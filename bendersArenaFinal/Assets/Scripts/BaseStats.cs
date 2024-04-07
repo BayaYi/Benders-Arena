3af5ad7f08416c2f80f8666d9c21c5a85cd89401
BayaYi <75089683+BayaYi@users.noreply.github.com>
2024-04-07 15:53:54 +0300

    name değişikliği

--- bendersArenaFinal/Assets/Scripts/BaseStats.cs
+++ bendersArenaFinal/Assets/Scripts/BaseStats.cs
@@ -4,38 +4,37 @@ using UnityEngine;
 
 public class BaseStats : MonoBehaviour
 {
-    
-        
+     
 }
 public struct BaseStat 
 {
     //Bükme Yeteneði
     //0 diðer 1 ateþ 2 su 3 toprak 4 hava
-    public int bendingAbility { get; set; }
+    public int BendingAbility { get; set; }
     //Temel Statlar
-    public int strength { get; set; }
-    public int dexterity { get; set; }
-    public int constitution { get; set; }
-    public int intelligence { get; set; }
-    public int physicalPower { get; set; }
-    public int chiPower { get; set; }
-    public int charisma { get; set; }
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int Constitution { get; set; }
+    public int Intelligence { get; set; }
+    public int PhysicalPower { get; set; }
+    public int ChiPower { get; set; }
+    public int Charisma { get; set; }
     //Türetilmiþ Statlar
-    public int totalHealth { get; set; }
-    public int totalPhysicalEnergy { get; set; }
+    public int TotalHealth { get; set; }
+    public int TotalPhysicalEnergy { get; set; }
     public int TotalChiEnergy { get; set; }
-    public int totalSpeed { get; set; }
-    public int totalPhysicalDefence { get; set; }
-    public int totalBendingDefence { get; set; }
-    public int totalInitiative { get; set; }
+    public int TotalSpeed { get; set; }
+    public int TotalPhysicalDefence { get; set; }
+    public int TotalBendingDefence { get; set; }
+    public int TotalInitiative { get; set; }
     //Güncel Durumlar
-    public int currentHealth { get; set; }
-    public int currentPhysicalEnergy { get; set; }
-    public int currentChiEnergy { get; set; }
-    public int currentSpeed { get; set; }
-    public int currentPhysicalDefence { get; set; }
-    public int currentBendingDefence { get; set; }
-    public int currentInitiative { get; set; }
+    public int CurrentHealth { get; set; }
+    public int CurrentPhysicalEnergy { get; set; }
+    public int CurrentChiEnergy { get; set; }
+    public int CurrentSpeed { get; set; }
+    public int CurrentPhysicalDefence { get; set; }
+    public int CurrentBendingDefence { get; set; }
+    public int CurrentInitiative { get; set; }
 }
 
 
