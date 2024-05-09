f4b7b13818fc12dc54aab886703658c98e9108f7
neslihan <20060376@stu.omu.edu.tr>
2024-05-09 14:15:18 +0300

    neslihan
    her bir element için karakter kağıdı oluşturdum ve her karaktere özgü matematiksel hesaplamaların kodunuz yazıp unityde bağladım.

---
+++ bendersArenaFinal/Assets/Code/ToprakGeçişCode.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToprakGeçişCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public void ToprakGeçişCodeButton()
+    {
+        // İkinci sahneyi yükleyin (sahne adını doğru şekilde değiştirin)
+        SceneManager.LoadScene(11);
+    }
+
+      public void AteşGeçişCodeButton()
+    {
+        // İkinci sahneyi yükleyin (sahne adını doğru şekilde değiştirin)
+        SceneManager.LoadScene(13);
+    }
+       public void SuGeçişCodeButton()
+    {
+        // İkinci sahneyi yükleyin (sahne adını doğru şekilde değiştirin)
+        SceneManager.LoadScene(14);
+    }
+
+        public void HavaGeçişCodeButton()
+    {
+        // İkinci sahneyi yükleyin (sahne adını doğru şekilde değiştirin)
+        SceneManager.LoadScene(15);
+    }
+
+
+
+
+}
