f97ad5c878e8da2d649df5ff4684fc3209fb0b0b
neslihan <20060376@stu.omu.edu.tr>
2024-07-03 17:04:56 +0300

    ness

---
+++ bendersArenaFinal/Assets/Code/OyuncuCode.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OyuncuCode : MonoBehaviour
+{
+    public void KarakterKağidi()
+    {
+        // İkinci sahneyi yükleyin (sahne adını doğru şekilde değiştirin)
+        SceneManager.LoadScene(12);
+    }
+    public void KarakterKağidigeri()
+    {
+        // İkinci sahneyi yükleyin (sahne adını doğru şekilde değiştirin)
+        SceneManager.LoadScene(16);
+    }
+}
