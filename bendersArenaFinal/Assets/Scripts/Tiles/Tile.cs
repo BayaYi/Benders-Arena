3fa1667d4d56be171af6af68a35a517917c64bc5
CookieAlt <sergenaltintas@hotmail.com>
2024-07-05 05:51:13 +0300

    Update Tile.cs

--- bendersArenaFinal/Assets/Scripts/Tiles/Tile.cs
+++ bendersArenaFinal/Assets/Scripts/Tiles/Tile.cs
@@ -81,7 +81,7 @@ public class Tile : MonoBehaviour
 
             }
             else {*/
-                if (UnitManager.Instance.SelectedUnit.Faction == Faction.Player && OccupiedUnit.Faction == Faction.Enemy && GameManager.Instance.GameState == GameState.AttackState && OccupiedUnit.OccupiedTile._atkRange)
+                if (UnitManager.Instance.SelectedUnit.Faction == Faction.Player && OccupiedUnit.Faction == Faction.Enemy && GameManager.Instance.GameState == GameState.AttackState && OccupiedUnit.OccupiedTile._atkRange.activeSelf)
                 {
                     
                     var enemy = (BaseEnemy)OccupiedUnit;
@@ -93,7 +93,7 @@ public class Tile : MonoBehaviour
                     GameManager.Instance.ChangeState(GameState.ChangeTurnOrder);
                 }
                 
-                else if(UnitManager.Instance.SelectedUnit.Faction == Faction.Enemy && OccupiedUnit.Faction == Faction.Player && GameManager.Instance.GameState == GameState.AttackState && OccupiedUnit.OccupiedTile._atkRange)
+                else if(UnitManager.Instance.SelectedUnit.Faction == Faction.Enemy && OccupiedUnit.Faction == Faction.Player && GameManager.Instance.GameState == GameState.AttackState && OccupiedUnit.OccupiedTile._atkRange.activeSelf)
                 {
                     
                         var player = (BasePlayer)OccupiedUnit;
